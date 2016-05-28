b0e6a74f03bd281fb02c587df394bf555029d867
Jordan Kasper <jordan@jordankasper.com>
2016-05-28 14:31:18 -0400

    Added basic hooks for building the index and searching

--- src/scripts/modules/search.js
+++ src/scripts/modules/search.js
@@ -4,6 +4,10 @@
     const INDEX_ITEM = 'tiyo-search-index';
     const TEMPLATE = 'build/templates/search.html';
 
+    let $ui = null;
+    let indexData = null;
+    let pageData = {};
+
     tiy.loadModule({
         name: 'search',
         navIcon: 'fa-search',
@@ -11,26 +15,66 @@
     });
 
     function main(data, elem) {
-        let indexData = null;
         try { indexData = JSON.parse(localStorage.getItem(INDEX_ITEM) || null); } catch(e) { /* let this go */ }
+        console.info('loading search module with indexData', indexData);
 
-        $.get(chrome.extension.getURL(TEMPLATE))
-            .then(function(html) {
-                elem.append(html);
-                addIndexAge(indexData, elem);
-            });
+        $ui = $(elem);
+        pageData = data;
+
+        if (pageData.path) {
+            $.get(chrome.extension.getURL(TEMPLATE))
+                .then(function(html) {
+                    $ui.append(html);
+                    addIndexAge(indexData);
+                    $ui.find('.tiyo-assistant-search-refresh').click(buildIndex);
+                    $ui.find('form').submit(doSearch);
+                });
+        } else {
+            $ui.append(
+                $('<p>').text('Currently search is only supported from a Path page.')
+            );
+        }
     }
 
-    function addIndexAge(indexData, elem) {
+    function addIndexAge(indexData) {
         let now = Date.now();
         let ageDays = -1;
 
-        if (indexData && indexData.createTime) {
-            ageDays = (now - indexData.createTime) / (1000 * 60 * 60 * 24);
+        if (indexData && indexData[pageData.path.id] && indexData[pageData.path.id].createTime) {
+            ageDays = (now - indexData[pageData.path.id].createTime) / (1000 * 60 * 60 * 24);
         }
 
-        elem.find('.tiyo-assistant-search-age time')
+        $ui.find('.tiyo-assistant-search-age time')
             .text( (ageDays > -1) ? (ageDays.toFixed(1) + ' days ago') : '(never)' );
+    }
+
+    function doSearch(e) {
+        e.preventDefault();
+        if (!indexData) {
+            return $ui.find('.tiyo-assistant-notice').text('There is no index, please build it!');
+        }
+
+
+    }
+
+    function buildIndex() {
+        $ui.find('.tiyo-assistant-search-refresh').attr('disabled', 'disabled');
+        $ui.find('.tiyo-assistant-notice').text('Recreating... this could take a while.');
+
+        //     {
+        //         "157": {
+        //             "createTime": 1234567890,
+        //             "index": {
+        //                 "indexed-word": {
+        //                     "lessons": [ "615-1804" ],
+        //                     "assignments": [ "615-1025" ]
+        //                 ]
+        //             }
+        //         }
+        //     }
+
+
+        // paths/157/units/965/lessons/2893
     }
 
 
