d91fde8026dda7166fc13d15f169b6b017512270
Russell Osborne <russell@theironyard.com>
2017-03-16 21:51:41 -0400

    Added ESlint, for auto correct.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "env": {
+        "browser": true,
+    },
+    "rules": {
+        "comma-dangle": "off"
+    }
+};
